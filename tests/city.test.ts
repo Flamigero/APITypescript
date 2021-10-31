fd92f4da25f2ef14b0f291da01d8d02776c5239d
Flama <carlosnjimenez97@gmail.com>
2021-10-31 10:22:50 +0100

    Finish city tests

--- tests/city.test.ts
+++ tests/city.test.ts
@@ -1,19 +1,54 @@
 import supertest from 'supertest';
 import Server from '../models/server';
 
-test('test city endpoint', async () => {
-    const server = new Server();
-    const app = server.app;
-    const api = supertest(app);
+describe('test city endpoint', () => {
+    let api: any;
 
-    const response = await api
-        .get('/api/city')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+    beforeEach(() => {
+        const server = new Server();
+        const app = server.app;
+        api = supertest(app);
+    })
 
-    expect(response.body.total).toBe(5);
-});
+    test('Get cities', async () => {
+        const response = await api
+            .get('/api/city')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    
+        expect(response.body.total).toBe(5);
+    });
 
-afterAll(() => {
+    test('Get city', async () => {
+        const response = await api
+            .get('/api/city/102')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
 
+        expect(response.body.city).toBe('Palma de Mallorca');
+    });
+
+    test('Post cities', async () => {
+        const response = await api
+            .post('/api/city')
+            .send({city: "Barcelona"})
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+            expect(response.body.msg).toBe('City already exist');
+    });
+
+    test('Put cities', async () => {
+        const response = await api
+            .put('/api/city/102')
+            .send({city: "Palma de Mallorca"})
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+            expect(response.body.city.city).toBe('Palma de Mallorca');
+    });
+    
+    afterAll(() => {
+        
+    });
 });
